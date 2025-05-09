c085f8dad003a4437326b4ff60c93a4af5199bee
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-09 17:30:12 +0000

    template and update names

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: May 9, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  let answer = 0
+  let integer = parseInt(document.getElementById("integer").value)
+
+  for (let counter = 1; counter <= integer; counter++) {
+    answer = answer + counter
+  }
+  document.getElementById('result').innerHTML = "<p>The product is " + answer + ".</p>"
+}
